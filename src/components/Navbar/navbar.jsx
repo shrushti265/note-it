2a639946cda0c9761987d32ff852e9aae4e5dbc3
shrushti265 <shruy26@gmail.com>
2023-07-30 20:02:54 +0530

    added navbar

---
+++ src/components/Navbar/navbar.jsx
@@ -0,0 +1,10 @@
+import "./navbar.css"
+
+const Navbar = () => {
+    return (
+        <nav className="navbar flrex-row flex-align-center flex-justify-between">
+        <h5 className="logo">NoteIt</h5>
+        </nav>
+    )
+}
+export {Navbar}
