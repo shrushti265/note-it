ca2f01ac54ba876ea5c799228517073da2349d01
shrushti265 <shruy26@gmail.com>
2023-08-03 23:23:21 +0530

    added notes List

--- src/components/Notes/NotesList/noteList.jsx
+++ src/components/Notes/NotesList/noteList.jsx
@@ -1,8 +1,11 @@
+import "notes.css"
+import {NotesItem} from "../NoteItem"
+
 const NotesList = ({notes}) => {
     return (
         <article className="notes-container flex-col flex-align-center flex-justify-start mx-auto">
             {
-                
+                notes.map(notes => <NotesItem key={notes._id} note={note}/>)
             }
         </article>
     )
